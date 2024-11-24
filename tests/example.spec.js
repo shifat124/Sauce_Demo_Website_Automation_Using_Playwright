5cd76209ce99995fb531f39c8fa305c1fab98155
shifat124 <shifatlaskor779@gmail.com>
2024-11-24 13:57:15 +0600

    modified in import line in basic file

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -15,5 +15,5 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible(); 
 });
