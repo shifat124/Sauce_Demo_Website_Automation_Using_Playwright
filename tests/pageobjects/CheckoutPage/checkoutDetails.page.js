310dc48df3590a692eaa56d27c37db4412328eb2
shifat124 <shifatlaskor779@gmail.com>
2025-03-29 22:41:08 +0600

    Checkout Page Test - Validate the checkout operation cancels after click on Cancel button of CheckOutPage

--- tests/pageobjects/CheckoutPage/checkoutDetails.page.js
+++ tests/pageobjects/CheckoutPage/checkoutDetails.page.js
@@ -2,6 +2,7 @@ class CheckoutDetailsPage {
     constructor(page) {
         this.page = page;
         this.finishCheckoutBtn = page.locator('.btn_action.cart_button');
+        this.cancelCheckoutBtn = page.locator('.cart_cancel_link.btn_secondary');
     }
 }
 export default CheckoutDetailsPage;
--- tests/specs/CheckoutModule/checkout.spec.js
+++ tests/specs/CheckoutModule/checkout.spec.js
@@ -58,4 +58,21 @@ test.describe('Checkout Page Test', () => {
         const actual = await successCheckoutPageObject.verifyCheckoutOperation();
         expect(actual).toBeTruthy();
     });
+    test('Validate the checkout operation cancels after click on Cancel button of CheckOutPage', async ({ page }) => {
+        const loginPageObject = new LoginPage(page);
+        await loginPageObject.verifyLogin(LoginData.valid_username, LoginData.valid_password);
+        const homePageObject = new HomePage(page);
+        await homePageObject.verifyclickallAddToCartBtn();
+        const commonPageObject = new CommonPage(page);
+        await commonPageObject.cartLogo.click();
+        const addToCartPageObject = new AddToCartPage(page);
+        await addToCartPageObject.checkoutBtn.click();
+        const checkoutPageObject = new CheckoutPage(page);
+        await checkoutPageObject.setUserCheckoutInfo(CheckoutData.user_first_name, CheckoutData.user_last_name, CheckoutData.postal_code);
+        await checkoutPageObject.continueCartInfoBtn.click();
+        const checkoutDetailsPageObject = new CheckoutDetailsPage(page);
+        await checkoutDetailsPageObject.cancelCheckoutBtn.click();
+        console.log('homePageUrl', page.url());
+        await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html');
+    });
 });
